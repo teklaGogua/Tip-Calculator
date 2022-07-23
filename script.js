d94a6f7fac5074b8ae89df6ba8a03b59eb85ed6a
tekla <tekla.gogua.2006@gmail.com>
2022-07-23 18:23:15 +0400

    Did 'custom' input functionality in script.js file

--- script.js
+++ script.js
@@ -8,20 +8,33 @@ const tip = document.getElementById('tip');
 const total = document.getElementById('total');
 const btnsPer = document.querySelectorAll('.btn--per');
 
+let persentage;
+
+// Calculates tips and total payment and dispays them
+const calculateDisplay = function (persentage) {
+  // All calculations
+  const payRes = bill.value / people.value;
+  const tipRes = payRes * (persentage / 100);
+  const totalRes = payRes + tipRes;
+
+  // display tip(per person) and total(per person)
+  tip.textContent = tipRes;
+  total.textContent = totalRes;
+};
+
 // Loop through Persentage buttons
 for (let i = 0; i < btnsPer.length; i++) {
   // Tip buttons' functionality
   btnsPer[i].addEventListener('click', function () {
     // Gets persentage(number with $ symbol), parses and returns just number
-    const persentage = parseFloat(btnsPer[i].value) / 100;
+    persentage = parseFloat(btnsPer[i].value);
 
-    // All calculations
-    const payRes = bill.value / people.value;
-    const tipRes = payRes * persentage;
-    const totalRes = payRes + tipRes;
-
-    // display tip(per person) and total(per person)
-    tip.textContent = tipRes;
-    total.textContent = totalRes;
+    calculateDisplay(persentage);
   });
 }
+
+// Input Custom's functionality
+custom.addEventListener('change', function () {
+  persentage = custom.value;
+  calculateDisplay(persentage);
+});
