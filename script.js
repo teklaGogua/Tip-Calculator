c6b0f47d555ca7ed3abbf7bcf60dc7a05c0d2a7e
tekla <tekla.gogua.2006@gmail.com>
2022-07-23 18:01:54 +0400

    Did 'Select Tip' buttons functionality in script.js file

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 'use strict';
 
+// Selecting elements
 const bill = document.getElementById('bill');
 const people = document.getElementById('people');
+const custom = document.getElementById('custom');
 const tip = document.getElementById('tip');
 const total = document.getElementById('total');
-const custom = document.getElementById('custom');
 const btnsPer = document.querySelectorAll('.btn--per');
 
+// Loop through Persentage buttons
 for (let i = 0; i < btnsPer.length; i++) {
+  // Tip buttons' functionality
   btnsPer[i].addEventListener('click', function () {
-    let persentage = btnsPer[i].value;
-    console.log(persentage);
+    // Gets persentage(number with $ symbol), parses and returns just number
+    const persentage = parseFloat(btnsPer[i].value) / 100;
+
+    // All calculations
+    const payRes = bill.value / people.value;
+    const tipRes = payRes * persentage;
+    const totalRes = payRes + tipRes;
+
+    // display tip(per person) and total(per person)
+    tip.textContent = tipRes;
+    total.textContent = totalRes;
   });
 }
