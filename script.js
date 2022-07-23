000dde2f762b879a515ae135f970688b324506a5
tekla <tekla.gogua.2006@gmail.com>
2022-07-23 18:39:38 +0400

    Did reset functionality and created starting conditions in script.js

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ const custom = document.getElementById('custom');
 const tip = document.getElementById('tip');
 const total = document.getElementById('total');
 const btnsPer = document.querySelectorAll('.btn--per');
+const reset = document.getElementById('reset');
 
 let persentage;
+
+// Starting conditions
+const init = function () {
+  bill.value = 0;
+  people.value = 0;
+  custom.value = '';
+  tip.textContent = '0.00$';
+  total.textContent = '0.00$';
+};
+init();
 
 // Calculates tips and total payment and dispays them
 const calculateDisplay = function (persentage) {
@@ -18,8 +29,8 @@ const calculateDisplay = function (persentage) {
   const totalRes = payRes + tipRes;
 
   // display tip(per person) and total(per person)
-  tip.textContent = tipRes;
-  total.textContent = totalRes;
+  tip.textContent = tipRes.toFixed(2);
+  total.textContent = totalRes.toFixed(2);
 };
 
 // Loop through Persentage buttons
@@ -35,6 +46,9 @@ for (let i = 0; i < btnsPer.length; i++) {
 
 // Input Custom's functionality
 custom.addEventListener('change', function () {
-  persentage = custom.value;
+  persentage = parseFloat(custom.value);
   calculateDisplay(persentage);
 });
+
+// Button Reset's functionality
+reset.addEventListener('click', init);
